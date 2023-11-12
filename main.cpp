3871b8dabdfcf3039a1f0fe048076fae405f0dd5
Gamma King <61883930+GammaKing2000@users.noreply.github.com>
2023-11-12 14:37:51 +0530

    Update main.cpp
    added GPS parsing and code documentation.

--- main.cpp
+++ main.cpp
@@ -24,6 +24,8 @@
 #define VibraMotor 4
 #define OnboardLED 5
 
+#define BAUDRATE 115200
+
 HardwareSerial LTE_Serial(2);
 Audio audio;
 
@@ -44,6 +46,13 @@ char findJson[400];
 void receiveATCommand(int flag);
 void sendATCommand(String command);
 
+/**
+ * Toggles the state of a pin at a specified interval.
+ * 
+ * @param pin the pin number on the Arduino board to which the vibration motor is
+ * connected. It is of type uint8_t, which is an 8-bit unsigned integer.
+ * @param interval the time duration in milliseconds between each vibration.
+ */
 void vibrate(uint8_t pin, long interval)
 {
   unsigned long currentMillis = millis();
@@ -52,51 +61,70 @@ void vibrate(uint8_t pin, long interval)
   {
     previousMillis = currentMillis;
     if (digitalRead(pin) == HIGH)
-    {
       digitalWrite(pin, LOW);
-    }
     else
-    {
       digitalWrite(pin, HIGH);
-    }
   }
 }
 
+/**
+ * Sends an AT command to a device using a serial connection.
+ * 
+ * @param command a string that represents the AT command to be sent.
+ */
 void sendATCommand(String command)
 {
-  Serial.print("Query:    ");
+  Serial.print("Query: ");
   Serial.println(command);
-  LTE_Serial.println(command);
+  LTE_Serial.println(command); // Sends AT command
 }
 
+/**
+ * Finds a pointer to the first occurrence of a '{' character in the given 
+ * `response` string, or `nullptr` if no '{' character is found.
+ * 
+ * @param response a pointer to a character array that represents the response 
+ * received from some source, such as a server or an API.
+ * 
+ * @return returns a pointer to the first occurrence of the character '{' in the input 
+ * `response` string. If the character is found, the function returns a pointer to that
+ * character. If the character is not found, the function returns a null pointer (`nullptr`).
+ */
 const char *parseResponse(const char *response)
 {
   const char *jsonStart = strchr(response, '{');
-
   if (jsonStart)
-  {
     return jsonStart;
-  }
   else
-  {
     return nullptr;
-  }
 }
 
+/**
+ * Same as the `parseResponse` function, but for GPS location.
+ * 
+ * @param response a pointer to a character array that represents the GPS response.
+ * 
+ * @return returns a pointer to the start of the JSON data in the `response` string. If a comma is 
+ * found in the `response` string, the function returns a pointer to the character immediately after 
+ * the comma. If no comma is found, the function returns a null pointer (`nullptr`).
+ */
 const char *parseLOCResponse(const char *response)
 {
   const char *jsonStart = strchr(response, ',') + 1;
-
   if (jsonStart)
-  {
     return jsonStart;
-  }
   else
-  {
     return nullptr;
-  }
 }
 
+/**
+ * Processes a JSON message by parsing it, extracting the "message" field, and returning
+ * it as a String.
+ * 
+ * @param jsonString a pointer to a character array that represents a JSON string.
+ * 
+ * @return a String object.
+ */
 String processJsonMessage(const char *jsonString)
 {
   if (jsonString)
@@ -113,9 +141,7 @@ String processJsonMessage(const char *jsonString)
         Serial.println(message);
       }
       else
-      {
         Serial.println("No message field in the JSON.");
-      }
       return temp;
     }
     else
@@ -125,37 +151,94 @@ String processJsonMessage(const char *jsonString)
     }
   }
   else
-  {
     Serial.println("No JSON part found in the response.");
-  }
   return "";
 }
 
+/**
+ * Sends an AT command to enable GGA NMEA sentences for GPS and waits for a response.
+ */
 void checkLOC()
 {
-  sendATCommand("AT+QGPS=1,30,50,1,300");
+  sendATCommand("AT+QGPSGNMEA=\"GGA\"");
   delay(1000);
-  receiveATCommand(0);
-  delay(500);
-  sendATCommand("AT+QGPSLOC=0");
-  delay(2000);
   receiveATCommand(3);
-  delay(500);
-  sendATCommand("AT+QGPSEND");
-  delay(1000);
-  receiveATCommand(0);
   delay(500);
 }
 
+/**
+ * Takes coordinates as string and an integer limit, and converts it into a float value representing
+ * the actual coordinates.
+ * 
+ * @param value a string that represents the coordinate value. It can contains * both whole numbers and
+ * decimal numbers, separated by a dot (.). The numbers before the dot represent degrees, and the numbers
+ * after the dot represent minutes.
+ * @param limit determines whether the sent coordinates are latitude or longitude.
+ * 
+ * @return a float value, which is the result of converting the input string value into a coordinate
+ * value.
+ */
+float actualCoord(String value, int limit)
+{
+  float a = 0;
+  float b = 0;
+  int counter1 = 1;
+  int counter2 = 10;
+  int flag1 = 0;
+  int flag2 = 0;
+  for (int i = 0; i < value.length(); i++)
+  {
+    float temp = float(value[i] - '0');
+    if (i < limit)
+      a = a * 10 + temp;
+    else
+    {
+      if (value[i] == '.')
+      {
+        flag1 = 1;
+      }
+      if (flag2 == 0 && flag1 == 0)
+      {
+        b = b * counter1 + temp;
+        counter1 = counter1 * 10;
+      }
+    }
+
+    if (flag2 == 1)
+    {
+      b = b + (temp / counter2);
+      counter2 = counter2 * 10;
+    }
+
+    if (flag1 == 1)
+    {
+      flag2 = 1;
+      flag1 = 0;
+    }
+  }
+  return a + (b / 60.0);
+}
+
+/**
+ * Takes a JSON string as input, extracts latitude and longitude values from it, converts them
+ * to proper coordinates, and publishes the message with the extracted data to an MQTT topic.
+ * 
+ * @param jsonString a pointer to a character array (string) that contains a JSON message.
+ */
 void Publish_Message(const char *jsonString)
 {
   String output = "";
   StaticJsonDocument<96> doc;
-  //          <UTC time> <ddmm.mmmm>  <ddmm,mmmm> <HDOP> <altitude>                  <date>
-  //+QGPSLOC: 061951.000, 3150.7223N, 11711.9293E, 0.7,    62.2,   2,000.00,0.0,0.0, 110513, 09
+  //                            <ddmm.mmmmm> <0ddmm,mmmmm>
+  //+QGPSNMEA: $GPGGA,141009.00,2824.04883,N,07721.32483,E,0,00,99.99,179.9,M,,M,,*7E
   String Lat = "";
   String Long = "";
+  String yaxis = "";
+  String xaxis = "";
 
+  while (*jsonString != ',')
+    jsonString++;
+  jsonString++;
   while (*jsonString != ',')
   {
     Lat = Lat + *jsonString;
@@ -164,12 +247,34 @@ void Publish_Message(const char *jsonString)
   jsonString++;
   while (*jsonString != ',')
   {
+    yaxis = yaxis + *jsonString;
+    jsonString++;
+  }
+  jsonString++;
+  while (*jsonString != ',')
+  {
     Long = Long + *jsonString;
     jsonString++;
   }
+  jsonString++;
+  while (*jsonString != ',')
+  {
+    xaxis = xaxis + *jsonString;
+    jsonString++;
+  }
+  jsonString++;
+
+  //always send 2 with lat and 3 with long for proper parsing
+  float LLat = actualCoord(Lat, 2);
+  float LLong = actualCoord(Long, 3);
+  if (yaxis == "S")
+    LLat = LLat * (-1);
+  if (xaxis == "W")
+    LLong = LLong * (-1);
+
   doc["DEVICE_ID"] = DEVICE_ID;
-  doc["LAT"] = Lat;
-  doc["LONG"] = Long;
+  doc["LAT"] = LLat;
+  doc["LONG"] = LLong;
 
   serializeJson(doc, output);
   int payloadSize = output.length();
@@ -180,6 +285,9 @@ void Publish_Message(const char *jsonString)
   receiveATCommand(0);
 }
 
+/**
+ * Publishes a JSON payload to an MQTT topic using AT commands when the device is booted.
+ */
 void Publish_LIVE_NOW()
 {
   String output = "";
@@ -198,6 +306,12 @@ void Publish_LIVE_NOW()
   receiveATCommand(0);
 }
 
+/**
+ * The function `receiveATCommand` receives AT commands and performs different actions based on the
+ * value of the `flag` parameter.
+ * 
+ * @param flag an integer that determines the mode of operation for the receiveATCommand function.
+ */
 void receiveATCommand(int flag)
 {
   // flag=0 means Net configuration mode
@@ -295,13 +409,14 @@ void receiveATCommand(int flag)
       }
       else
       {
+        // If no data is received, print "retry"
         Serial.println("retry");
         delay(1000); // Adjust delay as needed
       }
     }
   }
 
-  //publishing GPS location
+  //flag=3 means publishing GPS location
   else if (flag == 3)
   {
     if (LTE_Serial.available())
@@ -316,6 +431,9 @@ void receiveATCommand(int flag)
   }
 }
 
+/**
+ * Sends a series of AT commands to establish a connection to the network.
+ */
 void connectToNet()
 {
   sendATCommand("AT");
@@ -360,9 +478,16 @@ void connectToNet()
   delay(500);
 }
 
+/**
+ * Sends AT commands to power on and configure the GPS module.
+ */
 void connectToGPS()
 {
-  sendATCommand("AT+QGPSCFG=\"outport\",\"usbnmea\"");
+  sendATCommand("AT+QGPSPOWER=1");
+  delay(1000);
+  receiveATCommand(0);
+  delay(500);
+  sendATCommand("AT+QGPS=1");
   delay(1000);
   receiveATCommand(0);
   delay(500);
@@ -370,40 +495,11 @@ void connectToGPS()
   delay(1000);
   receiveATCommand(0);
   delay(500);
-  sendATCommand("AT+QGPSCFG=\"gpsnmeatype\",2");
-  delay(1000);
-  receiveATCommand(0);
-  delay(500);
-  sendATCommand("AT+QGPSCFG=\"glonassnmeatype\",0");
-  delay(1000);
-  receiveATCommand(0);
-  delay(500);
-  sendATCommand("AT+QGPSCFG=\"galileonmeatype\",0");
-  delay(1000);
-  receiveATCommand(0);
-  delay(500);
-  sendATCommand("AT+QGPSCFG=\"beidounmeatype\",0");
-  delay(1000);
-  receiveATCommand(0);
-  delay(500);
-  sendATCommand("AT+QGPSCFG=\"gnssnmeatype\",2");
-  delay(1000);
-  receiveATCommand(0);
-  delay(500);
-  sendATCommand("AT+QGPSCFG=\"gnssconfig\",0");
-  delay(1000);
-  receiveATCommand(0);
-  delay(500);
-  sendATCommand("AT+QGPSCFG=\"autogps\",0");
-  delay(1000);
-  receiveATCommand(0);
-  delay(500);
-  sendATCommand("AT+QGPSCFG=\"gnssconfig\",0");
-  delay(1000);
-  receiveATCommand(0);
-  delay(500);
 }
 
+/**
+ * The function connects to AWS and enters into a receive state permanently.
+ */
 void connectToAWS()
 {
   sendATCommand("AT+QMTCFG=\"recv/mode\",0,0,1");
@@ -460,10 +556,15 @@ void connectToAWS()
   vibrate(OnboardLED, 2000);
 }
 
+/**
+ * Initializes various pins and modules, including the microSD card, serial
+ * communication, vibration motor, user switch, onboard LED, and audio.
+ */
 void setup()
 {
-  Serial.begin(9600);
-  LTE_Serial.begin(115200, SERIAL_8N1, EC_RX, EC_TX);
+  // Set microSD Card CS as OUTPUT and set HIGH
+  Serial.begin(BAUDRATE);
+  LTE_Serial.begin(BAUDRATE, SERIAL_8N1, EC_RX, EC_TX);
   pinMode(33, OUTPUT);
   pinMode(VibraMotor, OUTPUT);
   pinMode(UserSwitch, INPUT);
@@ -474,7 +575,6 @@ void setup()
   delay(500);
   pinMode(SD_CS, OUTPUT);
   digitalWrite(SD_CS, HIGH);
-
   SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
 
   if (!SD.begin(SD_CS))
@@ -483,16 +583,17 @@ void setup()
     while (true)
       ;
   }
-
   audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-
   audio.setVolume(21);
-  
   connectToNet();
   connectToGPS();
   connectToAWS();
 }
 
+/**
+ * Continuously runs to check for AWS commands, plays music accordingly and
+ * sends the coordinates back when the music is stopped by pressing the button.
+ */
 void loop()
 {
   if (mainFlag == 0)
@@ -510,7 +611,7 @@ void loop()
 
   if (mainFlag == 1 && buttonState == LOW && lastButtonState == HIGH)
   {
-    Serial.println("BUTTON STOP");
+    Serial.print("BUTTON STOP");
     audio.stopSong();
     digitalWrite(VibraMotor, LOW);
     mainFlag = 0;
